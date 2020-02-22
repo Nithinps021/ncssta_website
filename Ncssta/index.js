fde36d8c5e2451fa6db5ae852d759e7b648a6837
Nithinps021 <nithinps021@gmail.com>
2020-02-22 23:30:03 +0530

    added responsive layouts

--- Ncssta/index.js
+++ Ncssta/index.js
@@ -1,1 +1,9 @@
+const navSlider = () => {
+  const menuIcon = document.querySelector(".menu");
+  const nav = document.querySelector(".nav-link ");
 
+  menuIcon.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+navSlider();

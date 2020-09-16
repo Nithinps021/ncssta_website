9a080e7ff7b856f192034936b63fccbdc2cc699c
sarinps007 <70061082+sarinps007@users.noreply.github.com>
2020-09-16 15:03:47 +0530

    correction made

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ const navSlider = () => {
 };
 navSlider();
 
+const popUp=document.querySelector(".box")
+const close=document.querySelector('.close')
+window.addEventListener('load',function(){
+  showPopUp();
+})
+
+function showPopUp(){
+  const time=1;
+  let i=0;
+  const timer=setInterval(() => {
+    i++;
+    if(i==time){
+      clearInterval(timer);
+      popUp.classList.add("show")
+    }
+    
+  }, 1000);
+}
+
+close.addEventListener('click',function(){
+  popUp.classList.remove("show")
+
+})
 // const menuButton(){
 //   document.getElementById('menu').style.display='none';
 //   document.getElementById()
